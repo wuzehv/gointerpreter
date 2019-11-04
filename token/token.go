046bda1e83d43b42aff8e6b9a97574a43f98c245
wuzehv <wuzehv@qq.com>
2019-11-04 10:24:06 -0500

    扩展词法器,兼容更多关键字和运算符

--- token/token.go
+++ token/token.go
@@ -18,8 +18,17 @@ const (
 	INT   = "INT"
 
 	// 运算符
-	ASSIGN = "="
-	PLUS   = "+"
+	ASSIGN   = "="
+	PLUS     = "+"
+	MINUS    = "-"
+	BANG     = "!"
+	ASTERISK = "*"
+	SLASH    = "/"
+
+	LT     = "<"
+	GT     = ">"
+	EQ     = "=="
+	NOT_EQ = "!="
 
 	// 控制字符
 	COMMA     = ","
@@ -32,12 +41,22 @@ const (
 	// 关键字, 这里的值并不是源码中的关键字, 关键字字面值单独定义在keywords中
 	FUNCTION = "FUNCTION"
 	LET      = "LET"
+	IF       = "IF"
+	ELSE     = "ELSE"
+	TRUE     = "TRUE"
+	FALSE    = "FALSE"
+	RETURN   = "RETURN"
 )
 
 // 单独维护一份关键字, 这里的关键字就是源码里面的关键字
 var keywords = map[string]TokenType{
-	"fn":  FUNCTION,
-	"let": LET,
+	"fn":     FUNCTION,
+	"let":    LET,
+	"if":     IF,
+	"else":   ELSE,
+	"true":   TRUE,
+	"false":  FALSE,
+	"return": RETURN,
 }
 
 func LookupIdent(ident string) TokenType {
